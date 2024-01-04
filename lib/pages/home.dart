1e2bddb46766615c6d531f47849d14c1e3290cac
banie <banie.alafaf112@gmail.com>
2024-01-04 20:48:26 +0700

    Commit Final

---
+++ lib/pages/home.dart
@@ -0,0 +1,290 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:crud_app/pages/emloyee.dart';
+import 'package:crud_app/service/database.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  TextEditingController namecontroller = TextEditingController();
+  TextEditingController agecontroller = TextEditingController();
+  TextEditingController locationcontroller = TextEditingController();
+  Stream? EmployeeStream;
+
+  getontheload() async {
+    EmployeeStream = await DatabaseMethods().getEmployeeDetails();
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    getontheload();
+    super.initState();
+  }
+
+  Widget allEmployeeDetails() {
+    return StreamBuilder(
+        stream: EmployeeStream,
+        builder: (context, AsyncSnapshot snapshot) {
+          return snapshot.hasData
+              ? ListView.builder(
+                  itemCount: snapshot.data.docs.length,
+                  itemBuilder: (context, index) {
+                    DocumentSnapshot ds = snapshot.data.docs[index];
+                    return Container(
+                      margin: const EdgeInsets.only(bottom: 5),
+                      child: Material(
+                        elevation: 20,
+                        borderRadius: BorderRadius.circular(10),
+                        child: Container(
+                          padding: const EdgeInsets.all(15),
+                          width: MediaQuery.of(context).size.width,
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(10)),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                children: [
+                                  Text(
+                                    "Name : " + ds["Name"],
+                                    style: GoogleFonts.montserrat(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.blue),
+                                  ),
+                                  const Spacer(),
+                                  GestureDetector(
+                                      onTap: () {
+                                        namecontroller.text = ds["Name"];
+                                        agecontroller.text = ds["Age"];
+                                        locationcontroller.text =
+                                            ds["location"];
+                                        EditEmployeeDetail(ds["Id"]);
+                                      },
+                                      child: const Icon(Icons.edit)),
+                                  const SizedBox(
+                                    width: 15.0,
+                                  ),
+                                  GestureDetector(
+                                    onTap: () async {
+                                      await DatabaseMethods()
+                                          .deleteEmployeeDetail(ds["Id"]);
+                                    },
+                                    child: const Icon(
+                                      Icons.delete,
+                                      color: Color(0xffff4f5a),
+                                    ),
+                                  )
+                                ],
+                              ),
+                              Text(
+                                "Age : " + ds["Age"],
+                                style: GoogleFonts.montserrat(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.blue),
+                              ),
+                              Text(
+                                "Location : " + ds["location"],
+                                style: GoogleFonts.montserrat(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.blue),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    );
+                  })
+              : Container();
+        });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const Employee(),
+            ),
+          );
+        },
+        child: const Icon(Icons.add),
+      ),
+      appBar: AppBar(
+          title: Row(
+        children: [
+          Text(
+            'Data ',
+            style: GoogleFonts.montserrat(
+                fontSize: 30, fontWeight: FontWeight.bold, color: Colors.black),
+          ),
+          Text(
+            'App',
+            style: GoogleFonts.montserrat(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                color: const Color(0xffff4f5a)),
+          )
+        ],
+      )),
+      body: Container(
+        margin: const EdgeInsets.only(right: 15, left: 15),
+        child: Column(
+          children: [Expanded(child: allEmployeeDetails())],
+        ),
+      ),
+    );
+  }
+
+  Future EditEmployeeDetail(String id) => showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+            content: SingleChildScrollView(
+              controller: ScrollController(),
+              child: Container(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.pop(context);
+                          },
+                          child: const Icon(Icons.cancel),
+                        ),
+                        const SizedBox(
+                          width: 60,
+                        ),
+                        Text(
+                          'Edit',
+                          style: GoogleFonts.montserrat(
+                              fontSize: 24, fontWeight: FontWeight.bold),
+                        ),
+                        const SizedBox(
+                          width: 10.0,
+                        ),
+                        Text(
+                          'Details',
+                          style: GoogleFonts.montserrat(
+                              fontSize: 24, fontWeight: FontWeight.bold),
+                        )
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 20.0,
+                    ),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Name',
+                          style: GoogleFonts.montserrat(
+                              fontSize: 16, fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 8.0,
+                    ),
+                    TextField(
+                      controller: namecontroller,
+                      decoration: InputDecoration(
+                          hintText: 'Input Your Name',
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(8))),
+                    ),
+                    const SizedBox(
+                      height: 16.0,
+                    ),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Age',
+                          style: GoogleFonts.montserrat(
+                              fontSize: 16, fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 8.0,
+                    ),
+                    TextField(
+                      controller: agecontroller,
+                      decoration: InputDecoration(
+                          hintText: 'Input Your Age',
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(8))),
+                    ),
+                    const SizedBox(
+                      height: 16.0,
+                    ),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Location',
+                          style: GoogleFonts.montserrat(
+                              fontSize: 16, fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 8.0,
+                    ),
+                    TextField(
+                      controller: locationcontroller,
+                      decoration: InputDecoration(
+                          hintText: 'Input Your Location',
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(8))),
+                    ),
+                    const SizedBox(
+                      height: 20.0,
+                    ),
+                    SizedBox(
+                      height: 50.0,
+                      width: MediaQuery.of(context).size.width,
+                      child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.blue,
+                            foregroundColor: Colors.white,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10.0),
+                            ),
+                          ),
+                          onPressed: () async {
+                            Map<String, dynamic> updateInfo = {
+                              "Name": namecontroller.text,
+                              "Age": agecontroller.text,
+                              "Id": id,
+                              "location": locationcontroller.text
+                            };
+                            await DatabaseMethods()
+                                .updateEmployeeDetail(id, updateInfo)
+                                .then((value) {
+                              Navigator.pop(context);
+                            });
+                          },
+                          child: const Text('Update Data')),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ));
+}
---
+++ lib/service/database.dart
@@ -0,0 +1,30 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class DatabaseMethods {
+  Future addEmployeeDetails(
+      Map<String, dynamic> employeeInfoMap, String id) async {
+    return await FirebaseFirestore.instance
+        .collection("Employee")
+        .doc(id)
+        .set(employeeInfoMap);
+  }
+
+  Future<Stream<QuerySnapshot>> getEmployeeDetails() async {
+    return await FirebaseFirestore.instance.collection("Employee").snapshots();
+  }
+
+  Future updateEmployeeDetail(
+      String id, Map<String, dynamic> updateInfo) async {
+    return await FirebaseFirestore.instance
+        .collection("Employee")
+        .doc(id)
+        .update(updateInfo);
+  }
+
+  Future deleteEmployeeDetail(
+      String id) async {
+    return await FirebaseFirestore.instance
+        .collection("Employee")
+        .doc(id).delete();
+  }
+}
